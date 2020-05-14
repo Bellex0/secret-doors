8c957ee62c016a372366196821e0096a77413eb6
bellex0 <bellepx3@gmail.com>
2020-05-14 06:22:07 -0400

    Added last sound

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Sound from 'react-sound';
+// import Sound from 'react-sound';
 import UIfx from 'uifx'
 import alien from '../music/alien.mp3'
 import tribal from '../music/tribalDrums.mp3'
+import drumCircle from '../music/drumCircle.mp3'
+import bell from '../music/bells.mp3'
 
 export default function Room() {
 
@@ -39,22 +41,35 @@ const tribalSong = new UIfx (
     }
   };
 
+  const drumCircleSong = new UIfx (
+      drumCircle, 
+      {
+        volume: 0.4, // number between 0.0 ~ 1.0
+        throttleMs: 100
+      }
+  )
 
+  const bellSong = new UIfx (
+      bell, {
+        volume: 0.4, // number between 0.0 ~ 1.0
+        throttleMs: 100
+      }
+  )
 
-    // const handleClick = (evt) => {
-    //     console.log(evt.target.id)
-    //     alienSong.toggleplay()
-    // }
-    
-    // return  <Sound 
-    //  url={alien}
-    //  playStatus={Sound.status.PLAYING}
-    //  />
+    const handleClick = () => {
+        return drumCircleSong.play()
+    }
+
+    const handleClick2 = () => {
+        return bellSong.play()
+    }
 
    return (
        <div id="button-grid">
-           <button id="btn1" onClick={togglePlay}>1</button>
-           <button id="btn2" onClick={togglePlay1}>2</button>
+           <button id="playBtn" onClick={togglePlay}>1</button>
+           <button id="playBtn" onClick={togglePlay1}>2</button>
+           <button id="playBtn" onClick={handleClick}>3</button>
+           <button id="playBtn" onClick={handleClick2}>4</button>
 
        </div>
    ) 
